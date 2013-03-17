8a105fcca92e85f310172a4c321f984947e1c8a4
Chris Talkington <chris@talkingtontech.com>
2013-03-17 17:02:55 -0500

    more mysql backup touchups.

--- tools/mysql-backup.sh
+++ tools/mysql-backup.sh
@@ -14,40 +14,44 @@ numdays=14
 #hotcopycmd=/usr/bin/mysqlhotcopy
 hotcopycmd="/usr/bin/mysqldump --lock-tables --databases"
 
+# Backup date format
 backupdate=`date +_%Y%m%d_%H%M`
 
 # Create directory if needed
 mkdir -p "$backupdir"
 if [ ! -d "$backupdir" ]; then
-   echo "Invalid directory: $backupdir"
-   exit 1
+  echo "Invalid directory: $backupdir"
+  exit 1
 fi
 
 # Hotcopy begins here
 echo "Dumping MySQL Databases..."
 RC=0
 for database in $dblist; do
-   echo
-   echo "Dumping $database ..."
-   $hotcopycmd $database | gzip > "$backupdir/$database$backupdate.sql.gz"
-
-   RC=$?
-   if [ $RC -gt 0 ]; then
-     continue;
-   fi
+  echo
+  echo "Dumping $database..."
+  $hotcopycmd $database | gzip > "$backupdir/$database$backupdate.sql.gz"
+  
+  RC=$?
+  if [ $RC -gt 0 ]; then
+    continue;
+  fi
 done
-
-echo "Removing Dumps Older Than $numdays Days..."
-find "$backupdir/" -type f -ctime "+$numdays" -exec rm -f {} \; -print
 
 echo
 if [ $RC -gt 0 ]; then
-   echo "MySQL Dump failed!"
-   exit $RC
+  echo "MySQL Dump failed!"
+  exit $RC
 else
-   # Hotcopy is complete. List the backup versions!
-   ls -la "$backupdir"
-   echo
-   echo "MySQL Dump is complete!"
+  echo "Removing Dumps Older Than $numdays Days..."
+  find "$backupdir/" -type f -ctime "+$numdays" -exec rm -f {} \; -print
+  
+  echo
+  echo "Listing Backup Directory Contents..."
+  ls -la "$backupdir"
+  
+  echo
+  echo "MySQL Dump is complete!"
 fi
+
 exit 0
